f4e0c3ebc357953ad4129b808e02cf0d19a78e69
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2025-05-16 00:54:19 +0200

    feat: added explanation section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {MonteCarloExplanationSection} from "@/components/MonteCarloExplanationSe
 
 export default function Home() {
     return (
-        <main className="container mx-auto px-4 py-8 max-w-4xl">
+        <main className="container mx-auto max-w-4xl">
             <MonteCarloExplanationSection/>
             <MonteCarloPiSimulation/>
         </main>
